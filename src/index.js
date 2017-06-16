c8f9c1839b8e8b228e6defcfd7bdcb3a55f45918
Brian Lauber <brian.lauber@33across.com>
2017-06-16 13:51:57 -0400

    Request details are airbraked

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 
-function notifyAirbrake(airbrake) {
+function notifyAirbrake(airbrake, options = {}) {
+  const defaultComponent = options.component || "koa";
+
   return function(ctx, next) {
     return next().catch((err)=> {
+      const req = ctx.request;
+      const res = ctx.response;
+
+      err.url = req.url;
+      err.action = req.url;
+      err.component = err.component || defaultComponent;
+      err.httpMethod = req.method;
+      err.params = req.body;
+      err.session = req.session;
+      err.ua = req.headers['User-Agent'];
+
       airbrake.notify(err);
       throw err;
     });
